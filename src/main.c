f13eb373938da1c4b42829dfd7c68cf9ad094c27
zichun zhang <zichun.zhang@ge1.iut-cachan.u-psud.fr>
2018-11-13 11:29:21 +0100

    j'ai fini exo2 du DS

--- src/main.c
+++ src/main.c
@@ -2,8 +2,24 @@
 //
 
 #include "my_header.h"
+#include <math.h>
+
+#define ECART 1e-10
 
 int main(void)
 {
+    double a = 1.0, b = 2.0, c = 3.0;
+    double delta;
+    unsigned char nb_solutions;
+
+    delta = b*b - 4.0 * a *c;
+
+    if ( fabs(delta) <  ECART )
+        nb_solutions = 1;
+    else if( delta > 0)
+        nb_solutions = 2;
+    else
+        nb_solutions = 0;
+
     return 0;
 }
