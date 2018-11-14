4a094dee9ca0ce9292ae3663644df03494c84703
zzhangge1 <44799595+zzhangge1@users.noreply.github.com>
2018-11-14 21:34:08 +0100

    Update exo _43_3

--- src/main.c
+++ src/main.c
@@ -2,8 +2,17 @@
 //
 
 #include "my_header.h"
+#include "stalib.h"
 
 int main(void)
 {
+    unsigned short int i = 1, j, S1 = 0;
+    
+    while (i<=12)
+    {
+        j = rand(10)+1;
+        S1 = S1 + j;
+        i = i + 1;
+    }
     return 0;
 }
