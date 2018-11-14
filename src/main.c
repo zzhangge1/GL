2ae94d68797b2ec3f84eef56fca8843caf2a21b8
zzhangge1 <44799595+zzhangge1@users.noreply.github.com>
2018-11-14 21:54:30 +0100

    I have changed exo_44_4

--- src/main.c
+++ src/main.c
@@ -2,8 +2,30 @@
 //
 
 #include "my_header.h"
+#include "math.h"
 
 int main(void)
 {
+    int val1 = 1,val2 = 1,a = 1,S = 0;
+    
+    while (a <=20 )
+    {
+        if (val1%3 == 0 || val1%7 == 0)
+        {
+            S = S + val1;
+            a = a + 1;
+            val1 = val1 + 1;
+            val2 = val2 + 1;
+        }
+        
+        else 
+        {
+            val1 = val1 + 1;
+            val2 = val2 + 1 ;
+            a = a + 1;
+        }
+            
+            
+            
     return 0;
 }
