6aa156f0ec75f03aef34c1bb2e67703222eaa11e
zichun zhang <zichun.zhang@ge1.iut-cachan.u-psud.fr>
2018-11-20 09:51:34 +0100

    i have changed exo_56

--- src/main.c
+++ src/main.c
@@ -1,9 +1,42 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
-
 #include "my_header.h"
-
+#define seuil 5
+#define seuilh 200
+#define seuilb -100
 int main(void)
-{
+{ 
+    unsigned char bin[10]={0,1,2,3,4,5,6,7,8,9},i;
+    for(i=0;i<10;i++)
+      if(bin[i] < seuil)
+         bin[i] = 0;
+      else
+         bin[i] = 1;
+	
+	
+		
+	int sat[10] ;
+	char o ;
+	
+	for (o=0;o<10;o++)
+		sat[o] = rand(); 
+		if(sat[o] > seuilh)
+			sat[o] = seuilh ;
+	    else if (bin[o] < seuilb)
+			sat[o] = seuilb  ;
+	    else
+			sat[o] = bin[o] ;
+		
+		
+    unsigned char bin[10]={0,1,2,3,4,5,6,7,8,9},p;
+	
+	for (p=0;p<10;p++)
+		
+	
+	
+	
+
+			
+
     return 0;
 }
