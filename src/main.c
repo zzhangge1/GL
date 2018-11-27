27d5fd728f305fecad034225d9f2bbbbd319cfc7
zzhangge1 <44799595+zzhangge1@users.noreply.github.com>
2018-11-27 20:34:00 +0100

    update exo58

--- src/main.c
+++ src/main.c
@@ -2,8 +2,27 @@
 //
 
 #include "my_header.h"
+#include "TP_lib_suite"
 
 int main(void)
 {
+    unsigned short int i,j,k,gagnante[6],joueur[6]={1,20,3,36,15,43},commun;
+    
+    for(i=0;i<100;i++)
+    {
+        intialiserTirage();
+        
+        for(j=0;j<6;j++)
+        {
+            gagnante[j] = tirerNumero();
+            
+            for(k=0;k<6,k++)
+            {
+                if(joueur[k] == gagnante[j]
+                    commun++;
+               
+            }
+        }
+    }
     return 0;
 }
