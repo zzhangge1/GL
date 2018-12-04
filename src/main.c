00419d45f139c3b77a416207d2cfbd1b5de5db85
zzhangge1 <44799595+zzhangge1@users.noreply.github.com>
2018-12-04 09:33:52 +0100

    update exo_79

--- src/main.c
+++ src/main.c
@@ -5,5 +5,23 @@
 
 int main(void)
 {
+    unsigned char nom[100],prenom[100],phrase[100];
+    unsigned long int annee_de_naissance;
+    
+    
+    printf("nom:");
+    scanf("%s",nom);
+    printf("prenom:");
+    scanf("%s",prenom);
+    printf("naissance:");
+     scanf("%lu",&annee_de_naissance);
+	while(1)
+    {
+    printf("Je m'appelle %s %s et j'ai %lu ans",nom,prenom,2018-annee_de_naissance);
+	}
+	
+    
+    
+    
     return 0;
 }
