215acb9e54a0b2962cccb135bf3d5a64b4b4b022
zzhangge1 <44799595+zzhangge1@users.noreply.github.com>
2018-12-18 09:48:47 +0100

    update main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,28 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
+#include "cmath.h"
+#include "cstudio.h" 
+#include "cstring.h" 
+#include "cstdint.h" 
+#include "my_header.hpp"
 
-#include "my_header.h"
 
-int main(void)
+int main (void)
 {
-    return 0;
+    long int anneau1,anneau2,anneau3,val_res;
+	
+	anneau1 = saisir_anneau(0,9,1);
+	anneau2 = saisir_anneau(0,9,2);
+	anneau3 = saisir_anneau(0,6,3);
+	
+	if(c>6)
+	R = -1;
+	
+    else 
+    {R = (10*a+b)*(uint16_t)pow(10,c);}
+	
+	printf("La valeur est: %d",R);
+	
+	return 0;
+	
 }
